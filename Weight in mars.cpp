ba0434bad0d3d25205484eadc9674fc40797b40f
Yousef-Elgendy <113798622+Yousef-Elgendy@users.noreply.github.com>
2022-11-12 21:26:21 +0200

    Add files via upload

---
+++ Weight in mars.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+  float w;
+  cout << "Enter your weight : ";
+   cin >> w;
+  cout << "Your weight in Mars is :  = "<< w*0.377;
+    return 0;
+}
